57b6c5eddef6a89bee886731cf216047518587cf
nfour <novus.nfour@gmail.com>
2021-03-21 11:12:39 +1000

    add cjs and mjs

---
+++ dist/es/xroute.d.ts
@@ -0,0 +1,98 @@
+import { History, Location } from 'history';
+/** Create a typed route config object */
+export declare const XRoute: <KEY extends string, RESOURCE extends string, PARAMS extends {}>(key: KEY, resource: RESOURCE, params: PARAMS) => {
+    key: KEY;
+    resource: RESOURCE;
+    params: PARAMS;
+};
+/**
+ * The Mobx class which handles routing over History.
+ */
+export declare class XRouter<LIST extends RouteConfig[], KEYS extends LIST[number]['key'], ROUTES extends {
+    [ITEM in LIST[number] as ITEM['key']]: LiveRoute<ITEM>;
+}, ROUTE_CONFIG extends RouteConfig> {
+    definition: LIST;
+    protected history: History;
+    location: Location;
+    dispose: () => void;
+    constructor(definition: LIST, history?: History);
+    /**
+     * A map of routes `{ [route.key]: route }`
+     *
+     * @example
+     *
+     * // Read parameters
+     * router.routes.myRoute.params?.myParam // string|undefined
+     *
+     * // Set the route and its parameters
+     * // Can be used to set a route from a different route too
+     * router.routes.myRoute.push({ myParam: 'banana' })
+     *
+     * // on myRoute now...
+     *
+     * router.routes.someOtherRoute.push({})
+     *
+     * // On someOtherRoute now.
+     *
+     * router.routes.routeWithRequired.push({
+     *   // router.route is always the activeRoute
+     *   myProp: router.route?.params?.myParam || 'something'
+     * })
+     */
+    get routes(): ROUTES;
+    /** The currently active route. */
+    get route(): ActiveLiveRoute<Required<NonNullable<ROUTES[KEYS]>>> | undefined;
+    toPath<ROUTE extends ROUTE_CONFIG>(route: ROUTE, params?: ROUTE['params']): string;
+    /** history.push() a given route */
+    push<ROUTE extends ROUTE_CONFIG>(route: ROUTE, params?: ROUTE['params']): void;
+    /** Equal to history.push(pathname) */
+    push(pathname: string): void;
+    /** history.replace() a given route */
+    replace<ROUTE extends ROUTE_CONFIG>(route: ROUTE, params?: ROUTE['params']): void;
+    /** Equal to history.replace(pathname) */
+    replace(pathname: string): void;
+    go: History['go'];
+    back: History['back'];
+    forward: History['forward'];
+    block: History['block'];
+    /**
+     * Be aware, toPath will throw if missing params.
+     * When navigating from another route, ensure you provide all required params.
+     */
+    protected navigate<ROUTE_DEF extends ROUTE_CONFIG>(route: ROUTE_DEF | string, params?: Partial<ROUTE_DEF['params']>, method?: 'push' | 'replace'): void;
+}
+export declare type RouteConfig = ReturnType<typeof XRoute>;
+/**
+ * A "live" route, typically found at:
+ * @example new XRouter(...).routes.myFooRoute
+ */
+export interface LiveRoute<ITEM extends RouteConfig> {
+    key: ITEM['key'];
+    resource: ITEM['resource'];
+    params?: ITEM['params'];
+    hash?: string;
+    search?: string;
+    index?: number;
+    path?: string;
+    isActive: boolean;
+    push(params?: Partial<ITEM['params']>): void;
+    pushExact(params: ITEM['params']): void;
+    replace(params?: Partial<ITEM['params']>): void;
+    replaceExact(params: ITEM['params']): void;
+    toPath(params?: Partial<ITEM['params']>): string;
+    toPathExact(params: ITEM['params']): string;
+}
+export interface ActiveLiveRoute<ITEM extends RouteConfig> extends LiveRoute<ITEM> {
+    params: ITEM['params'];
+    resource: ITEM['resource'];
+    key: ITEM['key'];
+    path: string;
+    index: number;
+    hash: string;
+    isActive: true;
+}
+/** Cast a list of LiveRoute[] to ActiveLiveRoute[]  */
+export declare function asActiveRoutes<ROUTE extends undefined | LiveRoute<any>>(routes: ROUTE[]): (ActiveLiveRoute<Required<NonNullable<ROUTE>>> | undefined)[];
+export declare function asActiveRoute<ROUTE extends undefined | LiveRoute<any>>(route: ROUTE): ActiveLiveRoute<Required<NonNullable<ROUTE>>> | undefined;
+/** Within LiveRoute[] find where isActive === true and return ActiveLiveRoute */
+export declare function findActiveRoute<ROUTE extends undefined | LiveRoute<any>>(routes: ROUTE[]): ActiveLiveRoute<Required<NonNullable<ROUTE>>> | undefined;
---
+++ dist/es/xroute.js
@@ -0,0 +1,191 @@
+import { createHashHistory } from 'history';
+import isEqual from 'lodash-es/isEqual';
+import { makeAutoObservable, reaction } from 'mobx';
+import { compile, match } from 'path-to-regexp';
+/** Create a typed route config object */
+export const XRoute = (key, resource, params) => ({ key, resource, params });
+/**
+ * The Mobx class which handles routing over History.
+ */
+export class XRouter {
+    constructor(definition, history = createHashHistory()) {
+        Object.defineProperty(this, "definition", {
+            enumerable: true,
+            configurable: true,
+            writable: true,
+            value: definition
+        });
+        Object.defineProperty(this, "history", {
+            enumerable: true,
+            configurable: true,
+            writable: true,
+            value: history
+        });
+        Object.defineProperty(this, "location", {
+            enumerable: true,
+            configurable: true,
+            writable: true,
+            value: void 0
+        });
+        Object.defineProperty(this, "dispose", {
+            enumerable: true,
+            configurable: true,
+            writable: true,
+            value: void 0
+        });
+        Object.defineProperty(this, "go", {
+            enumerable: true,
+            configurable: true,
+            writable: true,
+            value: (...args) => this.history.go(...args)
+        });
+        Object.defineProperty(this, "back", {
+            enumerable: true,
+            configurable: true,
+            writable: true,
+            value: () => this.history.back()
+        });
+        Object.defineProperty(this, "forward", {
+            enumerable: true,
+            configurable: true,
+            writable: true,
+            value: () => this.history.forward()
+        });
+        Object.defineProperty(this, "block", {
+            enumerable: true,
+            configurable: true,
+            writable: true,
+            value: (...args) => this.history.block(...args)
+        });
+        this.definition = definition;
+        this.history = history;
+        const setLocation = (location) => {
+            if (isEqual(this.location, location))
+                return;
+            this.location = { ...location };
+        };
+        const setHistory = (location) => {
+            if (isEqual(this.history.location, location))
+                return;
+            this.history.replace({ ...location });
+        };
+        const stopSettingHistory = reaction(() => this.location, (location) => {
+            setHistory(location);
+        });
+        const stopSettingLocation = history.listen(({ location }) => setLocation(location));
+        this.dispose = () => {
+            stopSettingLocation();
+            stopSettingHistory();
+        };
+        setLocation(history.location);
+        makeAutoObservable(this);
+    }
+    /**
+     * A map of routes `{ [route.key]: route }`
+     *
+     * @example
+     *
+     * // Read parameters
+     * router.routes.myRoute.params?.myParam // string|undefined
+     *
+     * // Set the route and its parameters
+     * // Can be used to set a route from a different route too
+     * router.routes.myRoute.push({ myParam: 'banana' })
+     *
+     * // on myRoute now...
+     *
+     * router.routes.someOtherRoute.push({})
+     *
+     * // On someOtherRoute now.
+     *
+     * router.routes.routeWithRequired.push({
+     *   // router.route is always the activeRoute
+     *   myProp: router.route?.params?.myParam || 'something'
+     * })
+     */
+    get routes() {
+        var _a;
+        const { pathname = '/', hash, search } = (_a = this.location) !== null && _a !== void 0 ? _a : {};
+        return this.definition.reduce((routes, _route) => {
+            const route = _route;
+            const { key, resource } = route;
+            const matched = match(resource, {
+                decode: decodeURI,
+                encode: encodeURI,
+            })(pathname);
+            const { index, path, params } = matched || {};
+            const mergeParams = (p = {}) => {
+                var _a, _b;
+                return ({
+                    ...((_b = (_a = this.route) === null || _a === void 0 ? void 0 : _a.params) !== null && _b !== void 0 ? _b : {}),
+                    ...p,
+                });
+            };
+            const newRoute = {
+                isActive: index !== undefined,
+                key,
+                index,
+                params,
+                resource,
+                path,
+                hash,
+                search,
+                push: (p) => this.push(route, mergeParams(p)),
+                pushExact: (p) => this.push(route, p),
+                replace: (p) => this.replace(route, mergeParams(p)),
+                replaceExact: (p) => this.replace(route, p),
+                toPath: (p) => this.toPath(route, mergeParams(p)),
+                toPathExact: (p) => this.toPath(route, p),
+            };
+            return { ...routes, [key]: newRoute };
+        }, {});
+    }
+    /** The currently active route. */
+    get route() {
+        if (!this.routes)
+            return;
+        // Get routes in order.
+        for (const { key } of this.definition) {
+            const route = this.routes[key];
+            if (route.isActive)
+                return asActiveRoute(route);
+        }
+    }
+    toPath(route, params) {
+        const { resource, key } = route;
+        try {
+            return compile(resource)({ ...params }) || '/';
+        }
+        catch (error) {
+            throw new Error(`INVALID_PARAMS\nROUTE: ${key}\nPATH: ${resource}\n ${error}`);
+        }
+    }
+    push(route, params) {
+        this.navigate(route, params, 'push');
+    }
+    replace(route, params) {
+        this.navigate(route, params, 'replace');
+    }
+    /**
+     * Be aware, toPath will throw if missing params.
+     * When navigating from another route, ensure you provide all required params.
+     */
+    navigate(route, params = {}, method = 'push') {
+        if (typeof route === 'string') {
+            return this.history[method](route);
+        }
+        const path = this.toPath(route, params);
+        this.history[method](path);
+    }
+}
+/** Cast a list of LiveRoute[] to ActiveLiveRoute[]  */
+export function asActiveRoutes(routes) {
+    return routes.map(asActiveRoute);
+}
+export function asActiveRoute(route) {
+    return route;
+}
+/** Within LiveRoute[] find where isActive === true and return ActiveLiveRoute */
+export function findActiveRoute(routes) {
+    return asActiveRoutes(routes).find((r) => r === null || r === void 0 ? void 0 : r.isActive);
+}
